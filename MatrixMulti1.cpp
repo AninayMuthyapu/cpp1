348e0e22d5a201235a18cdc2b4358f5ad32fb5cb
Aninay <aninay.work600@gmail.com>
2025-05-07 06:46:47 +0000

    Added random input values and separated matrix multiplication into a function

--- MatrixMulti1.cpp
+++ MatrixMulti1.cpp
@@ -1,33 +1,10 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 #include "anyoption.h"
 
-int main(int argc, char* argv[]) {
-    AnyOption opt;
-
-    opt.setOption("m");
-    opt.setOption("n");
-    opt.setOption("k");
-
-    
-
-    opt.processCommandArgs(argc, argv);
-
-    int m = opt.getValue("m") ? std::atoi(opt.getValue("m")) : 512;
-    int n = opt.getValue("n") ? std::atoi(opt.getValue("n")) : 512;
-    int k = opt.getValue("k") ? std::atoi(opt.getValue("k")) : 512;
-
-    std::cout << "Matrix A: " << m << "x" << k << "\n";
-    std::cout << "Matrix B: " << k << "x" << n << "\n";
-    std::cout << "Matrix C: " << m << "x" << n << "\n";
-
-    float* A = new float[m * k];
-    float* B = new float[k * n];
-    float* C = new float[m * n];
-
-    for (int i = 0; i < m * k; ++i) A[i] = 1.0f;
-    for (int i = 0; i < k * n; ++i) B[i] = 2.0f;
-
+// Matrix multiplication function
+void multiplyMatrices(float* A, float* B, float* C, int m, int n, int k) {
     for (int i = 0; i < m; ++i)
         for (int j = 0; j < n; ++j) {
             float sum = 0.0f;
@@ -35,8 +12,48 @@ int main(int argc, char* argv[]) {
                 sum += A[i * k + p] * B[p * n + j];
             C[i * n + j] = sum;
         }
+}
 
-    std::cout << "C[0][0] = " << C[0] << "\n";
+int main(int argc, char* argv[]) {
+    srand(time(0));  // Seed random generator
+
+    AnyOption opt;
+
+
+    opt.setOption("m");
+    opt.setOption("n");
+    opt.setOption("k");
+    opt.processCommandArgs(argc, argv);
+
+    int m = std::atoi(opt.getValue("m"));
+    int n = std::atoi(opt.getValue("n"));
+    int k = std::atoi(opt.getValue("k"));
+
+    std::cout << "Matrix A: " << m << "x" << k << std::endl;
+    std::cout << "Matrix B: " << k << "x" << n << std::endl;
+    std::cout << "Matrix C: " << m << "x" << n << std::endl;
+
+    float* A = new float[m * k];
+    float* B = new float[k * n];
+    float* C = new float[m * n];
+
+    // Fill A and B with random float values between 0 and 1
+    for (int i = 0; i < m * k; ++i)
+        A[i] = static_cast<float>(rand()) / RAND_MAX;
+    for (int i = 0; i < k * n; ++i)
+        B[i] = static_cast<float>(rand()) / RAND_MAX;
+
+    // Perform matrix multiplication
+    multiplyMatrices(A, B, C, m, n, k);
+
+    std::cout << "Matrix C (Result):" << std::endl;
+for (int i = 0; i < m; ++i) {
+    for (int j = 0; j < n; ++j) {
+        std::cout << C[i * n + j] << " ";
+    }
+    std::cout << std::endl;
+}
+
 
     delete[] A;
     delete[] B;
@@ -44,4 +61,3 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
-
