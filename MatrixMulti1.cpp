77eb9a668d38290175ef9028b22501da9e84e8d1
Aninay <aninay.work600@gmail.com>
2025-05-09 18:22:48 +0000

    Updated matrix multiplication with validation

--- MatrixMulti1.cpp
+++ MatrixMulti1.cpp
@@ -95,7 +95,7 @@ int main(int argc, char* argv[]) {
     float* A = new float[m * k];
     float* B = new float[k * n];
     float* C = new float[m * n];
-    float* C_omp = new float[m * n];
+    
     float* C_tiled = new float[m * n];
 
     
