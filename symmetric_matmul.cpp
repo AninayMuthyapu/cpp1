5df9144e9bfc91e9e1c9262f0b4cbbb57b0f1c63
AninayMuthyapu <Aninay.work600@gmail.com>
2025-07-02 19:07:40 +0530

    AVX changes

--- symmetric_matmul.cpp
+++ symmetric_matmul.cpp
@@ -1,4 +1,4 @@
-#include <cstddef>  
+#include <cstddef>
 #include <vector>
 #include <random>
 #include <chrono>
@@ -14,6 +14,35 @@
 
 using namespace std;
 using namespace std::chrono;
+
+inline void transpose8_ps(__m256 &row0, __m256 &row1, __m256 &row2, __m256 &row3, __m256 &row4, __m256 &row5, __m256 &row6, __m256 &row7) {
+    __m256 __t0, __t1, __t2, __t3, __t4, __t5, __t6, __t7;
+    __m256 __tt0, __tt1, __tt2, __tt3, __tt4, __tt5, __tt6, __tt7;
+    __t0 = _mm256_unpacklo_ps(row0, row1);
+    __t1 = _mm256_unpackhi_ps(row0, row1);
+    __t2 = _mm256_unpacklo_ps(row2, row3);
+    __t3 = _mm256_unpackhi_ps(row2, row3);
+    __t4 = _mm256_unpacklo_ps(row4, row5);
+    __t5 = _mm256_unpackhi_ps(row4, row5);
+    __t6 = _mm256_unpacklo_ps(row6, row7);
+    __t7 = _mm256_unpackhi_ps(row6, row7);
+    __tt0 = _mm256_shuffle_ps(__t0, __t2, _MM_SHUFFLE(1, 0, 1, 0));
+    __tt1 = _mm256_shuffle_ps(__t0, __t2, _MM_SHUFFLE(3, 2, 3, 2));
+    __tt2 = _mm256_shuffle_ps(__t1, __t3, _MM_SHUFFLE(1, 0, 1, 0));
+    __tt3 = _mm256_shuffle_ps(__t1, __t3, _MM_SHUFFLE(3, 2, 3, 2));
+    __tt4 = _mm256_shuffle_ps(__t4, __t6, _MM_SHUFFLE(1, 0, 1, 0));
+    __tt5 = _mm256_shuffle_ps(__t4, __t6, _MM_SHUFFLE(3, 2, 3, 2));
+    __tt6 = _mm256_shuffle_ps(__t5, __t7, _MM_SHUFFLE(1, 0, 1, 0));
+    __tt7 = _mm256_shuffle_ps(__t5, __t7, _MM_SHUFFLE(3, 2, 3, 2));
+    row0 = _mm256_permute2f128_ps(__tt0, __tt4, 0x20);
+    row1 = _mm256_permute2f128_ps(__tt1, __tt5, 0x20);
+    row2 = _mm256_permute2f128_ps(__tt2, __tt6, 0x20);
+    row3 = _mm256_permute2f128_ps(__tt3, __tt7, 0x20);
+    row4 = _mm256_permute2f128_ps(__tt0, __tt4, 0x31);
+    row5 = _mm256_permute2f128_ps(__tt1, __tt5, 0x31);
+    row6 = _mm256_permute2f128_ps(__tt2, __tt6, 0x31);
+    row7 = _mm256_permute2f128_ps(__tt3, __tt7, 0x31);
+}
 
 void compute_reference(float* A, float* B_full_matrix, float* C, int M, int N, int K) {
     memset(C, 0, M * N * sizeof(float));
@@ -31,7 +60,9 @@ void compute_reference(float* A, float* B_full_matrix, float* C, int M, int N, i
 bool verify_result(float* C_test, float* C_ref, int M, int N, float tolerance = 1e-3f) {
     for (int i = 0; i < M * N; ++i) {
         if (abs(C_test[i] - C_ref[i]) > tolerance) {
-            cout << "Mismatch at index " << i << ": Test=" << C_test[i] << " vs Ref=" << C_ref[i] << " (Diff=" << abs(C_test[i] - C_ref[i]) << ")" << endl;
+            cout << "Mismatch at index " << i << ": "
+                 << "Test=" << C_test[i] << " vs Ref=" << C_ref[i]
+                 << " (Diff=" << abs(C_test[i] - C_ref[i]) << ")" << endl;
             return false;
         }
     }
@@ -41,8 +72,8 @@ bool verify_result(float* C_test, float* C_ref, int M, int N, float tolerance = 
 template<int BM, int BN, int BK, int IT_M, int IT_N, int IT_K,
          bool MIsMultipleOfBM, bool NIsMultipleOfBN, bool KIsMultipleOfBK>
 void compute_matrix_multi1(float* A, float* B, float* C,
-                          int M, int N, int K, double& gflops, double& time_ms,
-                          float** C_cache, float** A_cache, float** B_cache) {
+                           int M, int N, int K, double& gflops, double& time_ms,
+                           float** C_cache, float** A_cache, float** B_cache) {
 
     constexpr int vector_width = 8;
     auto start = high_resolution_clock::now();
@@ -65,19 +96,30 @@ void compute_matrix_multi1(float* A, float* B, float* C,
                     memcpy(&local_A[i * BK], &A[(m1 + i) * K + k1], BK * sizeof(float));
                 }
 
-                
-
                 if (k1 >= n1) {
                     for (int kk = 0; kk < BK; ++kk) {
                         memcpy(&local_B[kk * BN], &B[(k1 + kk) * N + n1], BN * sizeof(float));
                     }
-                } else if (k1 + BK <= n1) {
+                } else if (k1 + BK <= n1 && BK == 8 && BN == 8 && k1 + 8 <= N && n1 + 8 <= N) {
+                    __m256 B_rows[8];
+                    for (int jj = 0; jj < 8; ++jj) {
+                        B_rows[jj] = _mm256_loadu_ps(&B[(n1 + jj) * N + k1]);
+                    }
+                    transpose8_ps(B_rows[0], B_rows[1], B_rows[2], B_rows[3], B_rows[4], B_rows[5], B_rows[6], B_rows[7]);
+                    for (int kk = 0; kk < 8; ++kk) {
+                        _mm256_storeu_ps(&local_B[kk * BN], B_rows[kk]);
+                    }
+                } else {
                     for (int jj = 0; jj < BN; ++jj) {
                         for (int kk = 0; kk < BK; ++kk) {
-                            local_B[kk * BN + jj] = B[(n1 + jj) * N + (k1 + kk)];
+                            int global_k = k1 + kk;
+                            int global_j = n1 + jj;
+                            int row = std::min(global_k, global_j);
+                            int col = std::max(global_k, global_j);
+                            local_B[kk * BN + jj] = B[row * N + col];
                         }
                     }
-                } 
+                }
 
                 for (int i = 0; i < BM; i += IT_M) {
                     for (int j = 0; j < BN; j += IT_N) {
@@ -97,7 +139,6 @@ void compute_matrix_multi1(float* A, float* B, float* C,
                             __m256 B_vec[IT_N / vector_width];
                             for (int nn = 0; nn < IT_N / vector_width; ++nn) {
                                 B_vec[nn] = _mm256_loadu_ps(&local_B[p * BN + j + nn * vector_width]);
-
                                 for (int mm = 0; mm < IT_M; ++mm) {
                                     C_tile[mm][nn] = _mm256_fmadd_ps(A_vec[mm], B_vec[nn], C_tile[mm][nn]);
                                 }
@@ -123,6 +164,7 @@ void compute_matrix_multi1(float* A, float* B, float* C,
     time_ms = duration<double, std::milli>(end - start).count();
     gflops = (2.0 * M * N * K) / (time_ms * 1e6);
 }
+
 void compute_openblas(float* A, float* B, float* C, int M, int N, int K, double& gflops, double& time_ms) {
     auto start = high_resolution_clock::now();
 
@@ -175,7 +217,8 @@ void testOpenBLAS(float* A, float* B, float* C_test, float* C_ref, int M, int N,
     results[idx][7] = is_correct ? 1.0f : 0.0f;
     idx++;
     cout << fixed << setprecision(3);
-    cout << "OpenBLAS | Time: " << avg_time_ms << " ms | Avg GFLOP/s: " << avg_gflops;
+    cout << "OpenBLAS | Time: " << avg_time_ms << " ms"
+         << " | Avg GFLOP/s: " << avg_gflops;
     if (check_results) {
         cout << " | " << (is_correct ? "PASS" : "FAIL");
     }
@@ -208,7 +251,6 @@ void testBlockSize(float* A, float* B, float* C_test, float* C_ref, int M, int N
 
     bool is_correct = true;
     if (check_results) {
-        compute_reference(A, B, C_ref, M, N, K);
         is_correct = verify_result(C_copy.data(), C_ref, M, N);
     }
 
@@ -248,8 +290,10 @@ void testBlockSize(float* A, float* B, float* C_test, float* C_ref, int M, int N
     idx++;
 
     cout << fixed << setprecision(3);
-    cout << "BMxBNxBK = " << BM << "x" << BN << "x" << BK
-         << " | IT_MxIT_NxIT_K = " << IT_M << "x" << IT_N << "x" << IT_K
+    cout << "BMxBNxBK = " << BM << "x" << BN
+         << "x" << BK
+         << " | IT_MxIT_NxIT_K = " << IT_M << "x"
+         << IT_N << "x" << IT_K
          << " | Time: " << avg_time_ms << " ms | Avg GFLOP/s: " << avg_gflops;
     if (check_results) {
         cout << " | " << (is_correct ? "PASS" : "FAIL");
@@ -267,9 +311,9 @@ int main(int argc, char* argv[]) {
     opt.setOption("k");
     opt.setOption("itr");
     opt.processCommandArgs(argc, argv);
-    
+
     int M = 1024, N = 1024, K = 1024, itr = 10;
-    
+
     if (opt.getValue("m") != NULL) M = atoi(opt.getValue("m"));
     if (opt.getValue("n") != NULL) N = atoi(opt.getValue("n"));
     if (opt.getValue("k") != NULL) K = atoi(opt.getValue("k"));
@@ -327,6 +371,6 @@ int main(int argc, char* argv[]) {
     testBlockSize<256, 128, 128, 4, 16, 1>(A.data(), B_matrix.data(), C_test.data(), C_ref.data(), M, N, K, itr, results, idx, check_results);
     testBlockSize<128, 64, 64, 4, 16, 1>(A.data(), B_matrix.data(), C_test.data(), C_ref.data(), M, N, K, itr, results, idx, check_results);
     testBlockSize<128, 256, 256, 4, 16, 1>(A.data(), B_matrix.data(), C_test.data(), C_ref.data(), M, N, K, itr, results, idx, check_results);
-  
+
     return 0;
 }
