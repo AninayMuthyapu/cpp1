61bdb6dc6ff9e54939b8136152770d189a314df3
AninayMuthyapu <Aninay.work600@gmail.com>
2025-07-18 19:09:22 +0530

    Added operations test

--- test/test_run.py
+++ test/test_run.py
@@ -1,21 +1,19 @@
 from dsl.var import Var
-from dsl.runner import run  # unified runner.py
+from dsl.runner import run  
 from dsl.matrix import GeneralMatrix
 import numpy as np
 
-# Define symbolic dimensions
+
 M = Var("M")
 N = Var("N")
 K = Var("K")
 
-# Define matrices
 A = GeneralMatrix((M, K))
 B = GeneralMatrix((K, N))
 C = A + B
 C.name = "C"
 
-# Input data
- # For reproducibility
+
 inputs = {
     "A": np.random.rand(4, 4).astype(np.float32),
     "B": np.random.rand(4, 4).astype(np.float32),
@@ -24,13 +22,13 @@ inputs = {
     "K": 4
 }
 
-# Choose backend explicitly: "openblas" or "numpy"
+
 result = run([C], inputs, backend="openblas")
 result2 = run([C], inputs, backend="cpp") 
 result3 = run([C], inputs, backend="numpy")
 
 
-# Print result
+
 print("Computed C (OpenBLAS):\n", result["C"])
 print("Computed C cpp:\n", result2["C"])
 print("Computed C numpy:\n", result3["C"])
