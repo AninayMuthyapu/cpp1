8c4c6a8b614684be6b58b4ead11f5fcd1d869f8b
AninayMuthyapu <Aninay.work600@gmail.com>
2025-08-27 20:29:39 +0530

    Added matmuexpression and for inverse

--- src/dsl/var.py
+++ src/dsl/var.py
@@ -1,17 +1,14 @@
-
 class Expression:
-    
     pass
 
 class Comparison(Expression):
-    
     def __init__(self, left, op, right):
         self.left = left
         self.op = op
         self.right = right
 
     def __repr__(self):
-        return f"Comparison({self.left}, '{self.op}', {self.right})"
+        return f"({self.left} {self.op} {self.right})"
 
     def __eq__(self, other):
         return (isinstance(other, Comparison) and
@@ -19,13 +16,15 @@ class Comparison(Expression):
                 self.left == other.left and
                 self.right == other.right)
 
+    def __hash__(self):
+        return hash((self.left, self.op, self.right))
+
 class Var(Expression):
-    
     def __init__(self, name):
         self.name = name
 
     def __repr__(self):
-        return f"Var('{self.name}')"
+        return self.name
 
     def __eq__(self, other):
         return isinstance(other, Var) and self.name == other.name
@@ -33,25 +32,26 @@ class Var(Expression):
     def __hash__(self):
         return hash(self.name)
 
-    
     def __add__(self, other):
         return ArithmeticExpression(self, '+', other)
     
     def __sub__(self, other):
         return ArithmeticExpression(self, '-', other)
 
-    
+    def __rsub__(self, other):
+        return ArithmeticExpression(other, '-', self)
 
     def __mul__(self, other):
         return ArithmeticExpression(self, '*', other)
 
     
+
     def __getitem__(self, index):
         return ArithmeticExpression(self, 'subscript', index)
 
-    
+    def __neg__(self):
+        return ArithmeticExpression(self, '-', None)
 
-    
     def __lt__(self, other):
         return Comparison(self, '<', other)
 
@@ -68,7 +68,6 @@ class Var(Expression):
         return Comparison(self, '==', other)
 
 class ArithmeticExpression(Expression):
-    
     def __init__(self, left, op, right):
         self.left = left
         self.op = op
@@ -113,11 +112,7 @@ class ArithmeticExpression(Expression):
     def __eq__(self, other):
         return Comparison(self, '==', other)
 
-    def __ne__(self, other):
-        return Comparison(self, '!=', other)
-
 class Conditional(Expression):
-    
     def __init__(self, condition, true_value, false_value):
         self.condition = condition
         self.true_value = true_value
@@ -125,3 +120,31 @@ class Conditional(Expression):
 
     def __repr__(self):
         return (f"({self.condition} ? {self.true_value} : {self.false_value})")
+
+class InverseExperession(Expression):
+    def __init__(self, operand_matrix,i,j):
+        self.operand_matrix = operand_matrix
+        self.i = i
+        self.j = j
+    def __repr__(self):
+        return f"Inverse({self.operand_matrix.name})[{self.i},{self.j}]"
+
+class Summation(Expression):
+    def __init__(self, expression, loop_var, start, end):
+        self.expression = expression
+        self.loop_var = loop_var
+        self.start = start
+        self.end = end
+
+    def __repr__(self):
+        return f"sum({self.loop_var}={self.start}..{self.end}, {self.expression})"
+
+class MatMulExpression(Expression):
+    def __init__(self, left_matrix_expr, right_matrix_expr, k_var, inner_dim_size_var):
+        self.left_matrix_expr = left_matrix_expr
+        self.right_matrix_expr = right_matrix_expr
+        self.k_var = k_var
+        self.inner_dim_size_var = inner_dim_size_var
+
+    def __repr__(self):
+        return f"MatMulExpression(left={self.left_matrix_expr}, right={self.right_matrix_expr})"
